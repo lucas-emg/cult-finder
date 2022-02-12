8184f66bfc92baca588b2aaa1b7acbffa1e1195e
Lucas Mendes <lucas.gabriel3.lg@gmail.com>
2022-02-12 15:37:10 -0300

    filter on the page is loading

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import { useState } from 'react'
 import TextField from '@mui/material/TextField';
 import Cards from '../components/Cards';
 import moviesList from '../movies.json'
@@ -7,18 +7,26 @@ const movies = moviesList.results
 
 const Search = () => {
 
+  const [searchTerm, setSearchTerm] = useState('')
+
   return (
     <div>
       <div className="search-header">
 
         <h2>Find the movie you'd like to watch</h2>
         
-        <TextField className="search-bar" id="standard-basic" label="Search your movie" variant="standard" />
+        <TextField onChange={e => {setSearchTerm(e.target.value)}} type="text"className="search-bar" id="standard-basic" label="Search your movie" variant="standard" />
       </div>
 
       <div className="search-results">
 
-        {movies.map((movie) => {
+        {movies.filter((val) => {
+          if(searchTerm === " ") {
+            return val
+          } else if (val.title.toLowerCase().includes(searchTerm.toLowerCase())) {
+            return val
+          }
+        }).map((movie) => {
           return <Cards poster={"https://image.tmdb.org/t/p/w780/" + movie.poster_path} title={movie.title} release={movie.release_date} sinopsis={movie.overview} />
         })}
 
