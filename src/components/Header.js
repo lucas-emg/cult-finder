436fc982bc3a4039b0f336b0e2a2d6e2bb0c2fe9
Erico Silva <tec.erico@yahoo.com.br>
2022-02-16 12:11:57 -0300

    header component is working

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+    <h1>Este é o componente Header.</h1>
+    <p>E ele fica na nossa pagina Home, com um texto bem legal.</p>
+    </div>
+  )
+}
+
+export default Header
