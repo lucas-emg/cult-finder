041703b5c8d739cee7c943b383e1cf4cfba39a70
Lucas Mendes <lucas.gabriel3.lg@gmail.com>
2022-02-25 15:44:51 -0300

    minor fixes on random movie

--- src/components/RandomMovie.js
+++ src/components/RandomMovie.js
@@ -48,8 +48,8 @@ const RandomMovie = () => {
       <div className="random-movie-text">
         <h3><span className="random-movie-title">{randomMovie.title}</span></h3>
         { randomMovie.overview
-          ? <p className='random-movie-details'><span className="text-bold">Sinopsis</span>: {randomMovie.overview}</p>
-          : <p className='random-movie-details'><span className="text-bold">Sinopsis</span>: No sinopsis available</p>}
+          ? <p className='random-movie-details'><span className="text-bold">Synopsis</span>: {randomMovie.overview}</p>
+          : <p className='random-movie-details'><span className="text-bold">Synopsis</span>: No sinopsis available</p>}
         { randomMovie.release_date
           ? <p className='random-movie-details'><span className="text-bold">Release</span>: {randomMovie.release_date}</p>
           : <p className='random-movie-details'><span className="text-bold">Release</span>: No release date available</p>}
